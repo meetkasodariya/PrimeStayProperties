4b1f0fec615269c492d5fb5be73e434610fbe373
meetkasodariya <mbhavsar590@gmail.com>
2025-04-17 11:18:03 +0530

    all

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,6 +1,6 @@
-const multer = require('multer');
-const cloudinary = require('cloudinary').v2; // Using v2 directly
-const streamifier = require('streamifier');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 // Configure Cloudinary
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME,
